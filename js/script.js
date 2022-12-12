a35876400627127601f9a219ddafb49c78821dae
FlorianKomnino <florian.komnino@outlook.it>
2022-12-12 17:54:08 +0100

    added VUE to setup repo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 console.log('Hello Universe!');
 
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}).mount ('#app')
